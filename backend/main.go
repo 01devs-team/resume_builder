a22c14a92659627a08bd859685709fcd0a141b7f
Ashkan Tofangdar <ashkan4472@gmail.com>
2022-11-08 00:34:58 +0330

    fix(backend): added initialization in main

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"resume_builder_backend/src/pkg/configs"
+	"resume_builder_backend/src/pkg/database"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
+// @title User API documentation
+// @version 1.0.0
+// @host localhost:3000
 func main() {
+	initialize()
+
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
 	app.Listen(":3000")
+}
+
+func initialize() {
+	configs.SetupEnv()
+	database.SetupPostgres()
 }
